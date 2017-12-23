7e5642d93bb805833e337bde54d868e663ac7964
Jodacame <jodacame@gmail.com>
2017-12-23 00:18:38 -0500

    Unicode

--- app.js
+++ app.js
@@ -67,14 +67,14 @@ pmx.initModule({
   Probe.metric({
     name : 'Uptime',
     value : function() {
-      return formatSeconds(stats.uptime);
+      return "⏱ "+formatSeconds(stats.uptime);
     }
   });
   Probe.metric({
     name : 'Connections',
     value : function() {
 
-      return stats.connections.current+" ("+((stats.connections.current*100)/stats.connections.available).toFixed(2)+"%) ["+stats.connections.avg+"/sec]";
+      return "💻 "+stats.connections.current+" ("+((stats.connections.current*100)/stats.connections.available).toFixed(2)+"%) ["+stats.connections.avg+"/sec]";
     },
     alert : {
       mode  : 'threshold',
